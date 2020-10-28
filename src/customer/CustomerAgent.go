420b498e76c519bb81831791c063f9cc1dfdedfe
Samuel <samuelfoley@live.ie>
2020-10-28 16:38:12 +0000

    Added item import and array

--- src/customer/CustomerAgent.go
+++ src/customer/CustomerAgent.go
@@ -1,23 +1,51 @@
-package customer
+package main
+
+import (
+	"fmt"
+	item "src/customer/item"
+)
 
 type CustomerAgent struct {
-	//Item
+	items            []item.ItemAgent
 	impairmentFactor float64
-	//replaceItem float64
-	//couponItem float64
-	//withChildren bool
-	//loyaltyCard bool
-	//baseAmicability float64
-	//customerAmicability float64
-	//preferredPayment float64
-	//avaliablePayment int[]
-	//baggintTimeSelfCheckout float64
-	//emergencyLeave float64
-	//switchLine float64
+	/* replaceItem             float64
+	couponItem              float64
+	withChildren            bool
+	loyaltyCard             bool
+	baseAmicability         float64
+	customerAmicability     float64
+	preferredPayment        float64
+	avaliablePayment        []int
+	baggintTimeSelfCheckout float64
+	emergencyLeave          float64
+	switchLine              float64 */
 }
 
-func newCustomer(impairmentFactor float64) *CustomerAgent {
+func newCustomer(items []item.ItemAgent, impairmentFactor float64) *CustomerAgent {
 
 	ca := CustomerAgent{impairmentFactor: impairmentFactor}
 	return &ca
+}
+
+func (customer *CustomerAgent) AddItem(item item.ItemAgent) {
+	customer.items = append(customer.items, item)
+}
+
+func main() {
+	item1 := *item.NewItem(false, 7.9)
+	item2 := *item.NewItem(true, 20.0)
+	item3 := *item.NewItem(false, 100.50)
+	var s []item.ItemAgent
+
+	customer1 := newCustomer(s, 60.0)
+	customer1.AddItem(item1)
+	customer1.AddItem(item2)
+	customer1.AddItem(item3)
+	fmt.Println(*customer1)
+	printSlice(customer1.items)
+
+}
+
+func printSlice(s []item.ItemAgent) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
--- src/customer/item/ItemAgent.go
+++ src/customer/item/ItemAgent.go
@@ -1,13 +1,11 @@
-package main
-
-import "fmt"
+package item
 
 type ItemAgent struct {
 	eighteenPlus bool
 	price        float64
 }
 
-func newItem(eighteenPlus bool, price float64) *ItemAgent {
+func NewItem(eighteenPlus bool, price float64) *ItemAgent {
 
 	ia := ItemAgent{eighteenPlus: eighteenPlus, price: price}
 	return &ia
@@ -17,7 +15,11 @@ func (item *ItemAgent) GetEighteenPlus() bool {
 	return item.eighteenPlus
 }
 
-func main() {
+func (item *ItemAgent) GetPrice() float64 {
+	return item.price
+}
+
+/*func main() {
 
 	fmt.Println(ItemAgent{eighteenPlus: true, price: 6.6})
 	fmt.Println(*newItem(true, 6.6))
@@ -26,5 +28,11 @@ func main() {
 	fmt.Println("Hello")
 
 	fmt.Println(item1.GetEighteenPlus())
+	fmt.Println(item1.GetPrice())
 
-}
+	item2 := newItem(true, 20.0)
+
+	fmt.Println(item2.GetEighteenPlus())
+	fmt.Println(item2.GetPrice())
+
+}*/
